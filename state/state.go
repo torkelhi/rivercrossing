d71102d0d10e2b047acaf0eb7471e675651a9667
torkelhi <torkelhi@uia.no>
2022-01-25 17:52:55 +0100

    brukte || ORE for å korte ned antall if-setninger

--- state/state.go
+++ state/state.go
@@ -15,69 +15,42 @@ var Status = HsState[0]
 
 func MoveTo(MoveTo string) string {
 
-	if MoveTo == "Restart" {
-		Status = HsState[0]
-		fmt.Println(HsState[0] + "Init State")
-		return HsState[0]
-	}
-	if MoveTo == "HS og kylling til Vest" {
+	if MoveTo == "Restart" || MoveTo == "HS og kylling til Vest" {
 		Status = HsState[0]
 		fmt.Println(HsState[0] + "Init State")
 		return HsState[0]
 	}
 
-	if MoveTo == "HS og kylling til båt" && Status == HsState[0] {
+	if MoveTo == "HS og kylling til båt" && (Status == HsState[0] || Status == HsState[2]) {
 		Status = HsState[1]
 		fmt.Println(HsState[1])
 		return HsState[1]
 	}
 
-	if MoveTo == "HS og kylling til båt" && Status == HsState[2] {
-		Status = HsState[1]
-		fmt.Println(HsState[1])
-		return HsState[1]
+	if MoveTo == "HS og kylling til Øst" && (Status == HsState[1] || Status == HsState[3]) {
+		Status = HsState[2]
+		fmt.Println(HsState[2])
+		return HsState[2]
 	}
 
-	if MoveTo == "HS og kylling til Øst" && Status == HsState[1] {
-		Status = HsState[2]
-		fmt.Println(HsState[2])
-		return HsState[2]
-	}
-	if MoveTo == "HS og kylling til Øst" && Status == HsState[3] {
-		Status = HsState[2]
-		fmt.Println(HsState[2])
-		return HsState[2]
-	}
-	if MoveTo == "HS til båt" && Status == HsState[2] {
+	if MoveTo == "HS til båt" && (Status == HsState[2] || Status == HsState[4]) {
 		Status = HsState[3]
 		fmt.Println(HsState[3])
 		return HsState[3]
 	}
-	if MoveTo == "HS til båt" && Status == HsState[4] {
-		Status = HsState[3]
-		fmt.Println(HsState[3])
-		return HsState[3]
-	}
-	if MoveTo == "HS til Vest" && Status == HsState[3] {
+
+	if MoveTo == "HS til Vest" && (Status == HsState[3] || Status == HsState[5]) {
 		Status = HsState[4]
 		fmt.Println(HsState[4])
 		return HsState[4]
 	}
-	if MoveTo == "HS til Vest" && Status == HsState[5] {
-		Status = HsState[4]
-		fmt.Println(HsState[4])
-		return HsState[4]
-	}
-	if MoveTo == "HS og korn til båt" && Status == HsState[4] {
+
+	if MoveTo == "HS og korn til båt" && (Status == HsState[4] || Status == HsState[6]) {
 		Status = HsState[5]
 		fmt.Println(HsState[5])
 		return HsState[5]
 	}
-	if MoveTo == "HS og korn til båt" && Status == HsState[6] {
-		Status = HsState[5]
-		fmt.Println(HsState[5])
-		return HsState[5]
-	}
+
 	if MoveTo == "HS og korn til Øst" && Status == HsState[5] {
 		Status = HsState[6]
 		fmt.Println(HsState[6] + "Du fikk alle over trygt og forlot reven, godt jobbet")
